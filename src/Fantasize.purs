97c70116f968bb32a9ec541b5fdc245f9f728cb4
joneshf <jones3.hardy@gmail.com>
2014-04-30 22:10:39 -0700

    Rest of the hierarchy.

--- src/Fantasize.purs
+++ src/Fantasize.purs
@@ -28,3 +28,30 @@ module Fantasize where
     \    };\
     \  };\
     \}" :: forall a f. (Functor f) => f a -> {}
+
+  foreign import fantasyApply
+    "function fantasyApply(dict) {\
+    \  return function(f) {\
+    \    f.constructor.prototype.ap = function(x) {\
+    \      return dict['<*>'](this)(x);\
+    \    };\
+    \  };\
+    \}" :: forall a f. (Apply f) => f a -> {}
+
+  foreign import fantasyApplicative
+    "function fantasyApplicative(dict) {\
+    \  return function(f) {\
+    \    f.constructor.prototype.of = function(x) {\
+    \      return dict['pure'](x);\
+    \    };\
+    \  };\
+    \}" :: forall a f. (Applicative f) => f a -> {}
+
+  foreign import fantasyChain
+    "function fantasyChain(dict) {\
+    \  return function(m) {\
+    \    m.constructor.prototype.chain = function(x) {\
+    \      return dict['>>='](this)(x);\
+    \    };\
+    \  };\
+    \}" :: forall a m. (Bind m) => m a -> {}
